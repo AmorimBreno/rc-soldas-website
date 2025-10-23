86f9cff774614cb582b451e9fd3c4a3562ae9426
Breno Amorim <brenoamorim11@gmail.com>
2025-10-23 10:45:35 -0300

    feat: forms details

--- src/app/components/ContactMeForms.tsx
+++ src/app/components/ContactMeForms.tsx
@@ -1,15 +1,14 @@
-import React from 'react';
-import { FormsData } from '../types/formsData';
-
-
+import React from 'react'
+import { FormsData } from '../types/formsData'
 
 type FormClienteProps = {
-  formsData: FormsData;
-  handleFormsChange: (e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void;
-  handleImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
-  handleFormsSubmit: (e: React.FormEvent<HTMLFormElement>) => void;
-
-};
+  formsData: FormsData
+  handleFormsChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void
+  handleImageChange: (e: React.ChangeEvent<HTMLInputElement>) => void
+  handleFormsSubmit: (e: React.FormEvent<HTMLFormElement>) => void
+}
 
 const FormCliente: React.FC<FormClienteProps> = ({
   formsData,
@@ -18,14 +17,14 @@ const FormCliente: React.FC<FormClienteProps> = ({
   handleFormsSubmit
 }) => {
   return (
-    <form onSubmit={handleFormsSubmit} className="flex flex-col gap-4 p-4">
+    <form onSubmit={handleFormsSubmit} className="grid grid-cols-2 gap-4 p-4">
       <input
         type="text"
         name="fullname"
         value={formsData.fullname}
         onChange={handleFormsChange}
         placeholder="Nome completo"
-        className="border p-2"
+        className="border-b-2 border-b-yellow-400 p-2"
       />
       <input
         type="text"
@@ -33,7 +32,7 @@ const FormCliente: React.FC<FormClienteProps> = ({
         value={formsData.company}
         onChange={handleFormsChange}
         placeholder="Empresa"
-        className="border p-2"
+        className="border-b-2 border-b-yellow-400 p-2"
       />
       <input
         type="text"
@@ -41,38 +40,39 @@ const FormCliente: React.FC<FormClienteProps> = ({
         value={formsData.number}
         onChange={handleFormsChange}
         placeholder="Telefone"
-        className="border p-2"
-        />
+        className="border-b-2 border-b-yellow-400 p-2"
+      />
       <input
         type="email"
         name="email"
         value={formsData.email}
         onChange={handleFormsChange}
         placeholder="Email"
-        className="border p-2"
+        className="border-b-2 border-b-yellow-400 p-2"
       />
       <textarea
         name="details"
         value={formsData.details}
         onChange={handleFormsChange}
         placeholder="Detalhes"
-        className="border p-2"
+        className="border-b-2 border-b-yellow-400 p-2"
       />
-      
+
       <input
+        placeholder="Images"
         type="file"
         name="images"
         multiple
         accept="image/*"
         onChange={handleImageChange}
-        className="border p-2"
+        className="grid-cols-2 border"
       />
 
-      <button type="submit" className="bg-blue-500 text-white p-2 rounded">
+      <button type="submit" className="rounded bg-blue-500 p-2 text-white">
         Enviar
       </button>
     </form>
-  );
-};
+  )
+}
 
-export default FormCliente;
+export default FormCliente
